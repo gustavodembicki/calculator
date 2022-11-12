d29b75796b7d19eee6ba88576d7693ced52a6e64
Gustavo Felipe Dembicki <gustavodembicki@hotmail.com>
2022-11-12 17:40:36 -0300

    initial commit
    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+import ContainerWrapper from './components/Container/Wrapper';
+
+export default function App() {
+  return <ContainerWrapper message='Hello World' />
+}
